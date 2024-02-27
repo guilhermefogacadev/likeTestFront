37571b31d2a3ccc5a5a2c36bcc656589afa320e0
Guilherme Fogaça <83102747+guilhermefogacadev@users.noreply.github.com>
2024-02-27 13:20:52 -0300

    up script,css,html and read me

---
+++ README.md
@@ -0,0 +1,22 @@
+# Front-End 
+
+Front-End da API `( https://github.com/guilhermefogacadev/likeTest ) ` para o teste técnico Like Sistema
+
+
+## Tecnologias Utilizadas
+
+- ![JavaScript](https://img.shields.io/badge/-JavaScript-yellow?logo=javascript&logoColor=white&style=flat-square)
+- ![HTML](https://img.shields.io/badge/-HTML-orange?logo=html5&logoColor=white&style=flat-square)
+- ![CSS](https://img.shields.io/badge/-CSS-blue?logo=css3&logoColor=white&style=flat-square)
+
+## Descrição do Projeto
+
+Este projeto consiste em uma aplicação web simples que demonstra o uso de JavaScript, HTML e CSS. No qual tem como finalidade criar orçamentos.
+
+## Como Executar
+
+1. Clone este repositório para sua máquina local.
+2. Abra com o live server, com a porta `http://127.0.0.1:5500`
+3. Automaticamente abrirá seu navegador web.
+
+
---
+++ script.js
@@ -0,0 +1,67 @@
+
+function salvarOrcamento() {
+  const nomeCliente = document.getElementById("nomeCliente").value;
+
+  const data = "";
+
+  const produtos = [];
+  const elementosProduto = document.querySelectorAll(".produto");
+  for (const elemento of elementosProduto) {
+    const produto = {
+      nome: elemento.querySelector("input[id^='nomeProduto']").value,
+      valor: elemento.querySelector("input[id^='valorProduto']").value,
+      quantidade: elemento.querySelector("input[id^='quantidadeProduto']").value,
+    };
+    produtos.push(produto);
+  }
+
+  const dadosJson = {
+    nomeCliente,
+    data,
+    produtos,
+  };
+
+  const jsonStr = JSON.stringify(dadosJson, null, 2);
+
+
+  fetch('http://localhost:8080/api/orcamento/criar', {
+    method: 'post',
+    body: jsonStr,
+    headers: {
+      'Content-Type': 'application/json', 
+  },
+  })
+  .then(async (response) => {
+    if (response.status === 201) {
+      alert("Orçamento salvo com sucesso!");
+    } else {
+      const erro = await response.text();
+      alert("Erro ao salvar orçamento: " + erro);
+    }
+  })
+  .catch((error) => {
+    alert("Erro ao salvar orçamento: " + error.message);
+  });
+}
+
+  
+  
+  
+let indiceProduto = 1; 
+
+function adicionarProduto() {
+  const produtos = document.getElementById("produtos");
+  const novoProduto = `
+    <div class="produto">
+      <label for="nomeProduto${indiceProduto}">Nome do Produto:</label>
+      <input type="text" id="nomeProduto${indiceProduto}" name="nomeProduto${indiceProduto}" required>
+      <label for="valorProduto${indiceProduto}">Valor:</label>
+      <input type="number" id="valorProduto${indiceProduto}" name="valorProduto${indiceProduto}" required>
+      <label for="quantidadeProduto${indiceProduto}">Quantidade:</label>
+      <input type="number" id="quantidadeProduto${indiceProduto}" name="quantidadeProduto${indiceProduto}" required>
+    </div>
+  `;
+
+  produtos.innerHTML += novoProduto;
+  indiceProduto++;
+}
